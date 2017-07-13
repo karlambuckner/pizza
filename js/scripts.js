8beb53125ad30385488bb49458e0da4d6664ec9a
Karla Buckner <kmb2662@gmail.com>
2017-07-12 20:27:57 -0700

    Polish up indention.

--- js/scripts.js
+++ js/scripts.js
@@ -5,40 +5,41 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.price = function(userSelection) {
-    var Total=0; // base value
-    var inp = userSelection;
-    for (var i=0, sum=0;i < inp.length; i++) {
-      if (inp[i].checked) {
-        Total+=parseInt(inp[i].value)
+  var Total=0; // base value
+  var inp = userSelection;
+  for (var i=0, sum=0;i < inp.length; i++) {
+    if (inp[i].checked) {
+      Total+=parseInt(inp[i].value)
       }
     }
-    return Total;
+
+  return Total;
   }
 
-  Pizza.prototype.Selections = function(userSelection) {
-      var Values = "";
-      var inp = userSelection;
-      for (var i=0; i < inp.length; i++) {
-        if (inp[i].checked) {
-          Values+=inp[i].id + " ";
-        }
-      }
-      return Values;
+Pizza.prototype.Selections = function(userSelection) {
+  var Values = "";
+  var inp = userSelection;
+  for (var i=0; i < inp.length; i++) {
+    if (inp[i].checked) {
+      Values+=inp[i].id + " ";
     }
+  }
+  return Values;
+  }
 
 // User Interface//
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
-      event.preventDefault();
-      var userSelection = document.getElementsByTagName("input");
-      var newPizza = new Pizza();
-      var price = newPizza.price(userSelection);
-      var userChoices = newPizza.Selections(userSelection)
+    event.preventDefault();
+    var userSelection = document.getElementsByTagName("input");
+    var newPizza = new Pizza();
+    var price = newPizza.price(userSelection);
+    var userChoices = newPizza.Selections(userSelection)
 
     $(".show-price").hide();
     $(".show-price").show();
     $("#pizzaOrder").text(" " + userChoices);
     $("#pizzaPrice").text(" $" + price);
-
+    
   });
 });
